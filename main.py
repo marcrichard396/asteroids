5e833f75d72276ba29914f615c1fd181b67e2ff0
Arkay396 <mrichardgame@hotmail.com>
2024-10-18 21:44:30 -0400

    add Shot class

--- main.py
+++ main.py
@@ -1,18 +1,53 @@
 import pygame
 from constants import *
+from player import *
+from asteroidfield import *
+from shot import *
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    timer = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    Shot.containers = (shots, updatable, drawable)
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-        pygame.display.flip()
 
+
+        dt = timer.tick(60) / 1000
+        screen.fill("black")
+        
+        for member in updatable:
+            member.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collision(player) == True:
+                print("Game over!")
+                return
+
+        for member in drawable:
+            member.draw(screen)
+
+        pygame.display.flip()        
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
